173c76bda8023f13401f1056fe703ef3f4b78a7e
Musfirazia <42489563+Musfirazia@users.noreply.github.com>
2020-06-20 16:34:22 +0500

    Assignment 4 changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Dinner from './Dinner';
-
+import Message from './Message.js';
 function App() {
+
+let [count,setCount]=useState(1);
+let [isMorning,setisMorning]=useState(true);
   return (
-    <div className="App">
-      <Dinner dish="biryani" sweetdish="gulab jaman"/>
+    <div className={`box${isMorning ? 'morning' :''}`} >
+      <Message count={count} />
+      <h1 >{isMorning ? 'Good Morning':'Good Evening'}</h1>
+    <button onClick={()=>setCount(++count)}>  Update Button </button>
+      <button onClick={()=>{setisMorning(!isMorning) }}>
+      {isMorning ? 'morning':'Evening'}</button>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Message(props)
+{
+return(
+    <div>
+        <h1>Counter:{props.count}</h1>
+    </div>
+)
+}
+export default Message
